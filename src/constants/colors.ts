4e4a976b45707e04127a13d5bb8e7eed6e1bcd7b
Amanda Duarte <amandaduuaarte@gmail.com>
2023-12-22 22:23:21 -0300

    chore: Add svg libs.

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  backgrounds: {
+    white: '#ffffff',
+    primary: '#3E2D8F',
+    secondary: '#9D52AC',
+    primaryDark: '#170569',
+  },
+  texts: {
+    primary: '#DDB130',
+    gray: '#D9D9D9',
+    white: '#FFFFFF',
+  },
+};
