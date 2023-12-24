a8b1609636efd8f6617290647cf1dcf0b3a60876
Amanda Duarte <amandaduuaarte@gmail.com>
2023-12-24 18:53:40 -0300

    feat: Add time to refresh query.

--- src/services/queryClient.ts
+++ src/services/queryClient.ts
@@ -1,3 +1,9 @@
 import {QueryClient} from 'react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1 * 60 * 60 * 1000, // 1 hour
+    },
+  },
+});
